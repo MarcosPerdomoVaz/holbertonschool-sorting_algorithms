eca7ab4ab591ec32fbc31261dfdcfa6445d349e9
Macj04 <miguelcoa49@gmail.com>
2023-05-23 05:49:29 -0700

    fix betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,7 +28,6 @@ void quick_sorty(int *array, int *p_array, size_t size, size_t p_size)
 	
 	if (!array || size == 0)
 		return;
-	
 	for (index = 0; index < end; index++)
 	{
 		if (array[end] > array[index])
@@ -36,20 +35,16 @@ void quick_sorty(int *array, int *p_array, size_t size, size_t p_size)
 			temp_num = array[index];
 			array[index] = array[target];
 			array[target] = temp_num;
-			target++;
-			
+			target++;		
 			if (index >= target)
 				print_array(p_array, p_size);
 		}
 	}
-	
 	temp_num = array[end];
 	array[end] = array[target];
 	array[target] = temp_num;
-	
 	if (end != target && array[index] != array[target])
 		print_array(p_array, p_size);
-	
 	quick_sorty(&array[0], p_array, target, p_size);
 	quick_sorty(&array[target + 1], p_array, end - target, p_size);
 }
